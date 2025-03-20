b802c142b72fe6f9f8b90da5c64cc49ee1509a55
Yusuf Shihata <yusufshihata2006@gmail.com>
2025-03-20 12:24:21 +0200

    finishing the training pipeline

--- train.py
+++ train.py
@@ -1,3 +1,5 @@
+from preprocessing import PreprocessingPipeline
+from mertics import MetricComputer
 from transformers import (
     AutoModelForTokenClassification,
     DataCollatorWithPadding,
@@ -6,7 +8,7 @@ from transformers import (
 )
 
 class TrainingPipeline:
-    def __init__(self):
+    def __init__(self, model, dataset):
         self.training_args = TrainingArguments(
             output_dir="./results",
             evaluation_strategy="epoch",
@@ -18,4 +20,25 @@ class TrainingPipeline:
             logging_dir="./logs",
         )
 
+        self.preprocessor = PreprocessingPipeline()
+        self.dataset = dataset.map(
+            self.preprocessor.tokenize_and_align_labels,
+            batched=True
+        )
+        
+        self.data_collator = DataCollatorWithPadding(tokenizer=self.preprocessor.tokenizer)
 
+        self.compute_metrics = MetricComputer().compute_metrics
+
+        self.trainer = Trainer(
+            model=model,
+            args=self.training_args,
+            train_dataset=dataset['train'],
+            eval_dataset=dataset['validation'],
+            tokenizer=self.preprocessor.tokenizer,
+            data_collator=self.data_collator,
+            compute_metrics=self.compute_metrics
+        )
+
+    def train(self):
+        self.trainer.train()
